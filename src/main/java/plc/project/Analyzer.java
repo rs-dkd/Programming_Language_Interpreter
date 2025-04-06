e67110b61ab76b774234d12a19168d7e2679ad47
d4julian <julianubico@gmail.com>
2025-04-06 02:09:53 -0400

    implement methods
    Took 1 hour 54 minutes

--- src/main/java/plc/project/Analyzer.java
+++ src/main/java/plc/project/Analyzer.java
@@ -1,5 +1,6 @@
 package plc.project;
 
+import java.beans.Expression;
 import java.lang.reflect.Parameter;
 import java.math.BigDecimal;
 import java.math.BigInteger;
@@ -27,24 +28,33 @@ public final class Analyzer implements Ast.Visitor<Void> {
 
     @Override
     public Void visit(Ast.Source ast) {
+        ast.getFields().forEach(this::visit);
+        ast.getMethods().forEach(this::visit);
+
+        boolean found = false;
         for (Ast.Method method : ast.getMethods()) {
             if (method.getName().equals("main")) {
+                found = true;
                 if (!method.getParameters().isEmpty()) {
                     throw new RuntimeException("The main method should not have any parameters.");
                 }
                 if (method.getReturnTypeName().isEmpty() || !method.getReturnTypeName().get().equals("integer")) {
                     throw new RuntimeException("The main method should return an integer.");
                 }
-                return null;
             }
         }
-        throw new RuntimeException("Main method not found.");
+        if (!found) throw new RuntimeException("Main method not found.");
+
+        return null;
     }
 
-    /* TODO: Additionally, throws a RuntimeException if: The value, if present, is not assignable to the Field. For a value to be assignable, its type must be a subtype of the Field's type, as defined above. */
     @Override
     public Void visit(Ast.Field ast) {
-        if (ast.getValue().isPresent()) visit(ast.getValue().get());
+        if (ast.getValue().isPresent()) {
+            visit(ast.getValue().get());
+            requireAssignable(Environment.getType(ast.getTypeName()), ast.getValue().get().getType());
+        }
+        else if (ast.getConstant()) throw new RuntimeException("Constant field must have an initial value assigned to it when the Field is declared.");
 
         this.scope.defineVariable(ast.getName(), ast.getName(), Environment.getType(ast.getTypeName()), ast.getConstant(), Environment.NIL);
         ast.setVariable(scope.lookupVariable(ast.getName()));
@@ -87,14 +97,23 @@ public final class Analyzer implements Ast.Visitor<Void> {
 
     @Override
     public Void visit(Ast.Statement.Expression ast) {
-        if (!(ast.getExpression() instanceof Ast.Expression.Function)) throw new RuntimeException("Expression is not a function");
+        visit(ast.getExpression());
+        if (!(ast.getExpression() instanceof Ast.Expression.Function)) 
+            throw new RuntimeException("Expression is not a function");
         return null;
     }
 
-    /* TODO: Additionally, throws a RuntimeException if: The value, if present, is not assignable to the variable (see Ast.Field for info).*/
     @Override
     public Void visit(Ast.Statement.Declaration ast) {
         if (ast.getValue().isPresent()) visit(ast.getValue().get());
+
+        Environment.Type type;
+        if (ast.getTypeName().isPresent()) type = Environment.getType(ast.getTypeName().get());
+        else if (ast.getValue().isPresent()) type = ast.getValue().get().getType();
+        else throw new RuntimeException("The variable's type is not present.");
+
+        if (ast.getTypeName().isPresent() && ast.getValue().isPresent())
+            requireAssignable(type, ast.getValue().get().getType());
 
         this.scope.defineVariable(
                 ast.getName(),
@@ -103,18 +122,26 @@ public final class Analyzer implements Ast.Visitor<Void> {
                 ast.getVariable().getConstant(),
                 Environment.NIL);
 
+        ast.setVariable(this.scope.lookupVariable(ast.getName()));
+        
         return null;
     }
 
     @Override
     public Void visit(Ast.Statement.Assignment ast) {
+        visit(ast.getReceiver());
+        visit(ast.getValue());
+
         if (!(ast.getReceiver() instanceof Ast.Expression.Access accessExpression))
             throw new RuntimeException("The receiver is not an Access expression");
 
+        if (accessExpression.getVariable().getConstant())
+            throw new RuntimeException("Cannot assign to a constant field.");
 
-        /* TODO: The value is not assignable to the receiver (see Ast.Field for info). */
+        requireAssignable(accessExpression.getVariable().getType(), ast.getValue().getType());
 
-        scope.defineVariable(accessExpression.getVariable().getName(), true, accessExpression.getVariable().getValue());
+        //scope.defineVariable(accessExpression.getVariable().getName(), true, accessExpression.getVariable().getValue());
+        
         return null;
     }
 
@@ -140,86 +167,222 @@ public final class Analyzer implements Ast.Visitor<Void> {
 
     @Override
     public Void visit(Ast.Statement.For ast) {
-        //Environment.getType(ast.)
-        //if (((Ast.Statement.For)ast.getInitialization())..getType() != Environment.Type.COMPARABLE)
+        if (ast.getInitialization() != null) {
+            visit(ast.getInitialization());
+            if (ast.getInitialization() instanceof Ast.Statement.Declaration declaration) {
+                if (declaration.getTypeName().isPresent()) {
+                    Environment.Type type = Environment.getType(declaration.getTypeName().get());
+                    if (type != Environment.Type.COMPARABLE)
+                        throw new RuntimeException("The identifier is not a Comparable type.");
+                } else {
+                    throw new RuntimeException("The identifier is not a Comparable type.");
+                }
+            }
+        }
 
+        visit(ast.getCondition());
         if (ast.getCondition().getType() != Environment.Type.BOOLEAN)
             throw new RuntimeException("The condition is not of type Boolean");
 
-        if (ast.getIncrement() != ast.get);
+
+        if (ast.getIncrement() != null) {
+            visit(ast.getIncrement());
+            if (ast.getInitialization() instanceof Ast.Statement.Declaration declaration &&
+                ast.getIncrement() instanceof Ast.Statement.Expression expression &&
+                expression.getExpression().getType() != declaration.getVariable().getType()) 
+                    throw new RuntimeException("The expression in the increment is not the same type as the identifier given at the start of the For signature.");
+        }
 
         if (ast.getStatements().isEmpty())
             throw new RuntimeException("The list of statements is empty.");
 
-        throw new UnsupportedOperationException();  // TODO
+        Scope scope = new Scope(this.scope);
+        Scope prev = this.scope;
+        this.scope = scope;
+        ast.getStatements().forEach(this::visit);
+        this.scope = prev;
+        return null;
     }
 
     @Override
     public Void visit(Ast.Statement.While ast) {
+        visit(ast.getCondition());
         if (ast.getCondition().getType() != Environment.Type.BOOLEAN)
             throw new RuntimeException("The value is not of type boolean");
 
+        Scope scope = new Scope(this.scope);
+        Scope prev = this.scope;
+        
+        this.scope = scope;
+
         ast.getStatements().forEach(this::visit);
+        
+        this.scope = prev;
         return null;
     }
 
     @Override
     public Void visit(Ast.Statement.Return ast) {
-        throw new UnsupportedOperationException();  // TODO
+        Ast.Expression expression = ast.getValue();
+        visit(expression);
+        Environment.Type returnType = this.method.getFunction().getReturnType();
+        Environment.Type valueType = expression.getType();
+        requireAssignable(returnType, valueType);
+        return null;
     }
 
     @Override
     public Void visit(Ast.Expression.Literal ast) {
         final Object literal = ast.getLiteral();
-        if (literal)
-        if (type == Environment.Type.NIL ||
-            type == Environment.Type.BOOLEAN ||
-            type == Environment.Type.CHARACTER ||
-            type == Environment.Type.STRING
-        ) return null;
+        if (literal == null) {
+            ast.setType(Environment.Type.NIL);
+            return null;
+        }
+        if (literal instanceof Boolean) {
+            ast.setType(Environment.Type.BOOLEAN);
+            return null;
+        }
+        if (literal instanceof Character) {
+            ast.setType(Environment.Type.CHARACTER);
+            return null;
+        }
+        if (literal instanceof String) {
+            ast.setType(Environment.Type.STRING);
+            return null;
+        }
 
-        if (type == Environment.Type.INTEGER)
-            try {
-                BigInteger.valueOf((long) ast.getLiteral());
-            } catch(Exception e) {
-                throw new RuntimeException(e);
-            }
+        if (literal instanceof BigInteger) {
+            BigInteger value = (BigInteger) literal;
+            if (value.compareTo(BigInteger.valueOf(Integer.MAX_VALUE)) > 0 || 
+                value.compareTo(BigInteger.valueOf(Integer.MIN_VALUE)) < 0) 
+                throw new RuntimeException("value is out of range of a Java int (32-bit signed int)");
+            ast.setType(Environment.Type.INTEGER);
+            return null;
+        }
 
-        if (type == Environment.Type.DECIMAL)
-            try {
-                new BigDecimal(ast.getLiteral().toString()).doubleValue();
-            } catch(Exception e) {
-                throw new RuntimeException(e);
-            }
+        if (literal instanceof BigDecimal) {
+            BigDecimal value = (BigDecimal) literal;
+            if (Double.isInfinite(value.doubleValue()) || Double.isNaN(value.doubleValue()))
+                throw new RuntimeException("value is out of range of a Java double value (64-bit signed float)");
+            ast.setType(Environment.Type.DECIMAL);
+            return null;
+        }
 
-        ast.setType();
-
-
-        return null;
+        throw new RuntimeException("The literal is not of a valid type");
     }
 
     @Override
     public Void visit(Ast.Expression.Group ast) {
-        throw new UnsupportedOperationException();  // TODO
+        visit(ast.getExpression());
+        if (!(ast.getExpression() instanceof Ast.Expression.Binary))
+            throw new RuntimeException("The contained expression is not a binary expression");
+        ast.setType(ast.getExpression().getType());
+        return null;
     }
 
     @Override
     public Void visit(Ast.Expression.Binary ast) {
-        throw new UnsupportedOperationException();  // TODO
+        visit(ast.getLeft());
+        visit(ast.getRight());
+        
+        switch (ast.getOperator()) {
+            case "AND", "OR" -> {
+                if (ast.getLeft().getType() != Environment.Type.BOOLEAN || ast.getRight().getType() != Environment.Type.BOOLEAN)
+                    throw new RuntimeException("Both operands must be Boolean.");
+                if (ast.getLeft().getType() != ast.getRight().getType())
+                    throw new RuntimeException("Result type will be Boolean.");
+                ast.setType(Environment.Type.BOOLEAN);
+                return null;
+            }
+            case "<", "<=", ">", ">=", "==", "!=" -> {
+                if (ast.getLeft().getType() != Environment.Type.COMPARABLE || ast.getRight().getType() != Environment.Type.COMPARABLE)
+                    throw new RuntimeException("Both operands must be Comparable and of the same type.");
+                if (ast.getLeft().getType() != ast.getRight().getType())
+                    throw new RuntimeException("Result type will be Boolean.");
+                ast.setType(Environment.Type.BOOLEAN);
+                return null;
+            }
+            case "+" -> {
+                if (ast.getLeft().getType() == Environment.Type.STRING || ast.getRight().getType() == Environment.Type.STRING) {
+                    ast.setType(Environment.Type.STRING);
+                    return null;
+                }
+
+                if (ast.getLeft().getType() != Environment.Type.INTEGER && ast.getLeft().getType() != Environment.Type.DECIMAL)
+                    throw new RuntimeException("The LHS must be an Integer/Decimal.");
+
+                if (ast.getRight().getType() != ast.getLeft().getType())
+                    throw new RuntimeException("Both the RHS and result types are the same as the LHS.");
+
+                ast.setType(ast.getLeft().getType());
+                return null;
+            }
+            case "-", "*", "/" -> {
+                if (ast.getLeft().getType() != Environment.Type.INTEGER && ast.getLeft().getType() != Environment.Type.DECIMAL)
+                    throw new RuntimeException("The LHS must be an Integer/Decimal.");
+
+                if (ast.getRight().getType() != ast.getLeft().getType())
+                    throw new RuntimeException("Both the RHS and result types are the same as the LHS.");
+
+                ast.setType(ast.getLeft().getType());
+                return null;
+            }
+        }
+
+        throw new RuntimeException("The operator is not valid");
     }
 
     @Override
     public Void visit(Ast.Expression.Access ast) {
-        throw new UnsupportedOperationException();  // TODO
+        Environment.Variable variable;
+        if (ast.getReceiver().isPresent()) {
+            visit(ast.getReceiver().get());
+            Environment.Type type = ast.getReceiver().get().getType();
+            variable = type.getField(ast.getName());
+        } else variable = this.scope.lookupVariable(ast.getName());
+
+        ast.setVariable(variable);
+        return null;
     }
 
     @Override
     public Void visit(Ast.Expression.Function ast) {
-        throw new UnsupportedOperationException();  // TODO
+        Environment.Function function;
+        if (ast.getReceiver().isPresent()) {
+            visit(ast.getReceiver().get());
+            Environment.Type type = ast.getReceiver().get().getType();
+            try {
+                function = type.getFunction(ast.getName(), ast.getArguments().size() + 1);
+            } catch (RuntimeException e) {
+                throw new RuntimeException("Function not found in the receiver's type.");
+            }
+        } else function = scope.lookupFunction(ast.getName(), ast.getArguments().size());
+
+        List<Environment.Type> types = function.getParameterTypes();
+        for (int i = 0; i < ast.getArguments().size(); i++) {
+            Ast.Expression expression = ast.getArguments().get(i);
+            visit(expression);
+            Environment.Type argType = expression.getType();
+            Environment.Type expectedType = types.get(ast.getReceiver().isPresent() ? i + 1 : i);
+            requireAssignable(expectedType, argType);
+        }
+
+        ast.setFunction(function);
+
+        return null;
     }
 
     public static void requireAssignable(Environment.Type target, Environment.Type type) {
-        throw new UnsupportedOperationException();  // TODO
+        if (target == type || target == Environment.Type.ANY) return;
+
+        if (target != Environment.Type.COMPARABLE)
+            throw new RuntimeException("Target type is not comparable.");
+
+        if (type == Environment.Type.INTEGER || type == Environment.Type.DECIMAL || 
+            type == Environment.Type.CHARACTER || type == Environment.Type.STRING)
+            return;
+
+        throw new RuntimeException("The target type is not assignable to the given type.");
     }
 
 }
